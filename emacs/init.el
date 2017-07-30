c42788275e460ae9c5743d039136c5a51286edb5
Arjun Kathuria <arjun.kathuria8@gmail.com>
2017-07-30 11:10:15 +0530

    adds emacs and git dotfiles

---
+++ emacs/init.el
@@ -0,0 +1,146 @@
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+
+;;; set gc limit
+(setq gc-cons-threshold 50000000)
+(setq gnutls-min-prime-bits 4096)
+
+(require 'package)
+
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+(package-initialize)
+;;;(package-refresh-contents)
+
+;; tabs to spaces
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+
+;;undo-tree global
+(global-undo-tree-mode 1)
+
+;;Fix the scrolling to keep point in the center:
+(setq scroll-conservatively 10000
+      scroll-preserve-screen-position t)
+
+;;Make tab key do indent first then completion.
+(setq-default tab-always-indent 'complete)
+
+;;Relevant Apropos
+(setq apropos-sort-by-scores t)
+
+;; Mp M-o to switch windows
+(global-set-key (kbd "M-o") 'other-window)
+
+ 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; CUSTOM MAJOR MODES
+
+;; adds js2-mode as the major mode for javscript shit.
+;;(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;;-------------------
+
+;; web-mode config
+(setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2)
+  (setq js-indent-level 2)
+;;---------------------
+
+;; GLOBAL enabled modes and hooks
+(require 'flycheck)
+
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'global-flycheck-mode)
+(add-hook 'prog-mode-hook 'vi-tilde-fringe-mode)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+
+;;----------------------
+
+;;FLYCHECK LINTERS
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+(flycheck-add-mode 'javascript-eslint 'js2-mode)
+(flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
+
+
+
+;; Neotree configurations
+(global-set-key [f8] 'neotree-toggle)
+
+;;-----------------------
+
+;; electric pair mode
+(electric-pair-mode 1)
+(setq electric-pair-pairs '(
+                            (?\" . ?\")
+                            (?\` . ?\`)
+                            (?\( . ?\))
+                            (?\{ . ?\})
+                            ) )
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-term-color-vector
+   [unspecified "#1e1e1e" "#cf6a4c" "#8f9d6a" "#f9ee98" "#7587a6" "#9b859d" "#7587a6" "#a7a7a7"] t)
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "5a7830712d709a4fc128a7998b7fa963f37e960fd2e8aa75c76f692b36e6cf3c" "aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" default)))
+ '(fci-rule-color "#373b41")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(global-subword-mode t)
+ '(global-visual-line-mode t)
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(package-selected-packages
+   (quote
+    (material-theme company-go ace-window go-mode projectile rainbow-delimiters restart-emacs git-gutter vi-tilde-fringe flycheck flycheck-pos-tip neotree indium js2-mode js2-refactor json-mode magit web-mode company-tern company-web company rainbow-mode yasnippet undo-tree surround monokai-alt-theme color-theme-sanityinc-solarized ag abyss-theme)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#cc6666")
+     (40 . "#de935f")
+     (60 . "#f0c674")
+     (80 . "#b5bd68")
+     (100 . "#8abeb7")
+     (120 . "#81a2be")
+     (140 . "#b294bb")
+     (160 . "#cc6666")
+     (180 . "#de935f")
+     (200 . "#f0c674")
+     (220 . "#b5bd68")
+     (240 . "#8abeb7")
+     (260 . "#81a2be")
+     (280 . "#b294bb")
+     (300 . "#cc6666")
+     (320 . "#de935f")
+     (340 . "#f0c674")
+     (360 . "#b5bd68"))))
+ '(vc-annotate-very-old-color nil)
+ '(winner-mode t))
