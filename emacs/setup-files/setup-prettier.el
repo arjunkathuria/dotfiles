9aa9c57e04022acf06dbd0e85f2150079b3cee5b
Arjun Kathuria <arjun.kathuria8@gmail.com>
2018-02-11 19:49:38 +0530

    fixes prettier not hooking up on mode start

--- emacs/setup-files/setup-prettier.el
+++ emacs/setup-files/setup-prettier.el
@@ -2,7 +2,7 @@
 ;;; make sure to install prettier globally by npm.
 ;;; npm install -g prettier
 
-(use-package prettier-js
+(use-package prettier-js-mode
   :hook (js2-mode js2-jsx-mode web-mode)
 )
 
