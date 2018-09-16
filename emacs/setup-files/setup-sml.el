547d5b27fdff5126d56399462e79af6adcb38da7
Arjun Kathuria <arjun.kathuria8@gmail.com>
2018-09-16 13:42:59 +0530

    adds setup file for smart mode line

---
+++ emacs/setup-files/setup-sml.el
@@ -0,0 +1,11 @@
+;; Configuration for smart-mode-line
+
+;;; Code:
+(use-package smart-mode-line
+  :init
+  (smart-mode-line-enable)
+  (setq sml/theme 'dark)
+  :ensure t
+)
+
+(provide 'setup-sml)
