80577b814688b0814b16cf8e4a705b88d68097ec
Arjun Kathuria <arjun.kathuria8@gmail.com>
2019-01-24 22:05:26 +0530

    adds the setup file for smart-mode-line aka sml

--- emacs/setup-files/setup-sml.el
+++ emacs/setup-files/setup-sml.el
@@ -2,10 +2,20 @@
 
 ;;; Code:
 (use-package smart-mode-line
-  :init
-  (smart-mode-line-enable)
-  (setq sml/theme 'dark)
   :ensure t
-)
+  :init (add-hook 'after-init-hook 'sml/setup)
+  :config
+  (setq sml/no-confirm-load-theme t)
+  (setq sml/theme 'atom-one-dark)
+  (column-number-mode t)
+  (line-number-mode t)
+;  (display-battery-mode t)
+  )
+
+;; (use-package smart-mode-line
+
+;;   (setq sml/theme 'dark)
+;;   :ensure t
+;;)
 
 (provide 'setup-sml)
