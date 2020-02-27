12503eeef710fc714746cbc94978fa654064fcfc
Arjun Kathuria <arjun.kathuria8@gmail.com>
2020-02-27 13:48:54 +0530

    adds the new init.el file, with lots of backlog changes + setup-lsp file

--- emacs/init.el
+++ emacs/init.el
@@ -9,32 +9,54 @@
                          ("gnu"       . "https://elpa.gnu.org/packages/")
                          ("melpa"     . "https://melpa.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")))
+(package-initialize)
 
-;;(package-initialize)
+;; Bootstrap 'use-package'
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
-;;; Load Use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+;;Load Use-package
 (eval-when-compile
   ;; Following line is not needed if use-package.el is in ~/.emacs.d
   ;;(add-to-list 'load-path "<path where use-package is installed>")
   (require 'use-package))
+(setq use-package-always-ensure t)
 
-;;; Load Files
+
+;; Load Files
 (add-to-list 'load-path "~/.emacs.d/elisp/")
 (add-to-list 'load-path "~/.emacs.d/setup-files/")
 
-(require 'general)
+;; GENERAL SETUP
+(require 'general-emacs-setup)
+
+;; REQUIRED PACKAGES
 
 (require 'setup-emmet)
+
 (require 'setup-company)
 (require 'setup-web-mode)
 (require 'setup-electric-pair-mode)
 (require 'setup-counsel)
 (require 'setup-prettier)
-(require 'setup-alchemist)
-(require 'setup-bm)
-(require 'setup-whichkey)
+
 (require 'setup-flycheck)
+(require 'setup-sml)
+(require 'setup-neotree)
 (require 'setup-ace-window)
+(require 'setup-whichkey)
+(require 'setup-bm)
+(require 'setup-whitespace-cleanup)
+;;(require 'setup-alchemist)
+(require 'setup-slime)
+(require 'setup-elpy)
+(require 'setup-lsp)
+;; OPTIONAL PACKAGES, chose to load on convenience
+;; these packages result in things mostly cosmetic.
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -48,21 +70,36 @@
  '(ansi-term-color-vector
    [unspecified "#151515" "#ff0086" "#00c918" "#aba800" "#3777e6" "#ad00a1" "#3777e6" "#d0d0d0"] t)
  '(beacon-color "#ff9da4")
- '(custom-enabled-themes '(base16-oceanicnext))
+ '(custom-enabled-themes (quote (base16-snazzy)))
  '(custom-safe-themes
-   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "7527f3308a83721f9b6d50a36698baaedc79ded9f6d5bd4e9a28a22ab13b3cb1" "5a7830712d709a4fc128a7998b7fa963f37e960fd2e8aa75c76f692b36e6cf3c" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default))
+   (quote
+    ("7220c44ef252ec651491125f1d95ad555fdfdc88f872d3552766862d63454582" "d5e13e2100e2a1167cd521558c5344809c154cc211ccd39a9c06bb83d254927c" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "bc99493670a29023f99e88054c9b8676332dda83a37adb583d6f1e4c13be62b8" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "7527f3308a83721f9b6d50a36698baaedc79ded9f6d5bd4e9a28a22ab13b3cb1" "5a7830712d709a4fc128a7998b7fa963f37e960fd2e8aa75c76f692b36e6cf3c" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
+ '(doom-modeline-mode t)
+ '(elpy-rpc-backend "jedi" t)
  '(fci-rule-color "#37474f")
- '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
- '(frame-background-mode 'dark)
- '(hl-paren-colors '("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11"))
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(flycheck-python-flake8-executable "python3")
+ '(frame-background-mode (quote dark))
+ '(hl-paren-colors
+   (quote
+    ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
  '(hl-sexp-background-color "#1c1f26")
+ '(ivy-posframe-style (quote frame-top-center))
+ '(jdee-db-active-breakpoint-face-colors (cons "#222228" "#819cd6"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#222228" "#5b94ab"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#222228" "#515462"))
  '(linum-format " %7i ")
+ '(objed-cursor-color "#e1c1ee")
  '(package-selected-packages
-   '(dashboard spacemacs-theme exec-path-from-shell whole-line-or-region lorem-ipsum company-irony irony cmake-ide cmake-mode company-c-headers company-rtags rtags edts nordless-theme color-theme-sanityinc-tomorrow company-erlang spacegray-theme overcast-theme dad-joke danneskjold-theme nord-theme erlang bm which-key markdown-mode arjen-grey-theme flyspell-popup flycheck-elixir alchemist elixir-mode web-mode vi-tilde-fringe use-package undo-tree surround rjsx-mode restart-emacs rainbow-mode rainbow-delimiters projectile prettier-js osx-dictionary neotree monokai-alt-theme material-theme magit json-mode js2-refactor github-modern-theme gitconfig-mode git-gutter flycheck-pos-tip emmet-mode counsel company-web company-tern color-theme-sanityinc-solarized base16-theme ag ace-window))
- '(tramp-syntax 'default nil (tramp))
+   (quote
+    (all-the-icons-ivy-rich use-package lsp-ui company-statistics company-c-headers company-rtags company-lsp lsp-haskell flycheck-haskell haskell-mode elpy pipenv company-jedi all-the-icons-ivy doom-modeline doom-themes slime-company brutalist-theme nofrils-acme-theme all-the-icons dashboard exec-path-from-shell whole-line-or-region lorem-ipsum company-irony irony edts nordless-theme color-theme-sanityinc-tomorrow company-erlang spacegray-theme overcast-theme dad-joke danneskjold-theme nord-theme erlang bm markdown-mode arjen-grey-theme flyspell-popup flycheck-elixir alchemist elixir-mode web-mode vi-tilde-fringe undo-tree surround rjsx-mode restart-emacs rainbow-mode rainbow-delimiters prettier-js osx-dictionary neotree material-theme magit json-mode js2-refactor gitconfig-mode git-gutter flycheck-pos-tip emmet-mode counsel company-web company-tern color-theme-sanityinc-solarized base16-theme ag ace-window)))
+ '(pdf-view-midnight-colors (cons "#c6c6c6" "#282b33"))
+ '(python-shell-interpreter "python3" t)
+ '(tramp-syntax (quote default) nil (tramp))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
-   '((20 . "#f36c60")
+   (quote
+    ((20 . "#f36c60")
      (40 . "#ff9800")
      (60 . "#fff59d")
      (80 . "#8bc34a")
@@ -79,8 +116,9 @@
      (300 . "#f36c60")
      (320 . "#ff9800")
      (340 . "#fff59d")
-     (360 . "#8bc34a")))
- '(vc-annotate-very-old-color nil))
+     (360 . "#8bc34a"))))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
