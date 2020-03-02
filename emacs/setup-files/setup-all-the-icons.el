f898ed2f7743d7599920a41b53f2e85fc06329f6
Arjun Kathuria <arjun.kathuria8@gmail.com>
2020-03-02 12:42:31 +0530

    Adds changes to all-the-icons setup file, dired and ivy now have icons : )

--- emacs/setup-files/setup-all-the-icons.el
+++ emacs/setup-files/setup-all-the-icons.el
@@ -1,9 +1,19 @@
-(use-package all-the-icons)
-
-(use-package all-the-icons-ivy
+(use-package all-the-icons
   :ensure t
+  :defer t
+  ;; Run the init initially only ONCE on a new system when you need to install the icon-fonts.
+  ;; :init (all-the-icons-install-fonts)
   :config
-  (all-the-icons-ivy-setup))
-
+  (use-package all-the-icons-ivy
+    :ensure t
+    :after all-the-icons
+    :init
+    (all-the-icons-ivy-setup)
+    )
+  (use-package all-the-icons-dired
+    :config
+    (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+    )
+  )
 
 (provide 'setup-all-the-icons)
