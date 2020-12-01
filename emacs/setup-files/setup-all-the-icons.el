4d78804e1a719343ce958ad29a19fd8a9a8cb347
Arjun Kathuria <arjun.kathuria8@gmail.com>
2020-12-02 00:04:35 +0530

    updates all-the-icons setup file

--- emacs/setup-files/setup-all-the-icons.el
+++ emacs/setup-files/setup-all-the-icons.el
@@ -3,17 +3,20 @@
   :defer t
   ;; Run the init initially only ONCE on a new system when you need to install the icon-fonts.
   ;; :init (all-the-icons-install-fonts)
-  :config
-  (use-package all-the-icons-ivy
-    :ensure t
-    :after all-the-icons
-    :init
-    (all-the-icons-ivy-setup)
-    )
-  (use-package all-the-icons-dired
-    :config
-    (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
-    )
+  ;;:config
+  )
+
+(use-package all-the-icons-ivy
+  :ensure t
+  :after all-the-icons
+  :init
+  (all-the-icons-ivy-setup)
+  )
+(use-package all-the-icons-dired
+  :hook
+  ('dired-mode . 'all-the-icons-dired-mode)
+  ;; :config
+  ;; (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
   )
 
 (provide 'setup-all-the-icons)
