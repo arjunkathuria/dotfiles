dad09609f99777eda30619ae3c8a438182d38b36
Arjun Kathuria <arjun.kathuria8@gmail.com>
2020-12-02 00:02:59 +0530

    adds nim-mode setup file

--- emacs/setup-files/setup-nim.el
+++ emacs/setup-files/setup-nim.el
@@ -1,13 +1,9 @@
 (use-package nim-mode
   :ensure t
-
-  :hook
-  (nim-mode . highlight-indentation-current-column-mode)
-
+  ;;:hook
+  ;;(nim-mode . highlight-indentation-current-column-mode)
   :config
   (setq nimsuggest-path "c:/Users/Arjun/.nimble/bin/")
-
-
   ;; (highlight-indentation-mode 1)
   ;; Just an example, by default these functions are
   ;; already mapped to "C-c <" and "C-c >".
@@ -18,9 +14,9 @@
   ;; This can prevent to edit them by accident.
   (when (string-match "/\.nimble/" (or (buffer-file-name) "")) (read-only-mode 1))
 
-  (flycheck-mode 1)
-  (auto-fill-mode 0)
-  (electric-indent-local-mode 0)
+;;  (flycheck-mode 1)
+;;  (auto-fill-mode 0)
+;;  (electric-indent-local-mode 0)
   )
 
 (provide 'setup-nim)
