9fde9db60f0bf49ad1c06eee225775c8c50f83c1
Arjun Kathuria <arjun.kathuria8@gmail.com>
2020-12-14 22:22:36 +0530

    updates setup nim file

--- emacs/setup-files/setup-nim.el
+++ emacs/setup-files/setup-nim.el
@@ -4,6 +4,8 @@
   ;;(nim-mode . highlight-indentation-current-column-mode)
   :config
   (setq nimsuggest-path "c:/Users/Arjun/.nimble/bin/")
+
+
   ;; (highlight-indentation-mode 1)
   ;; Just an example, by default these functions are
   ;; already mapped to "C-c <" and "C-c >".
@@ -13,10 +15,6 @@
   ;; Make files in the nimble folder read only by default.
   ;; This can prevent to edit them by accident.
   (when (string-match "/\.nimble/" (or (buffer-file-name) "")) (read-only-mode 1))
-
-;;  (flycheck-mode 1)
-;;  (auto-fill-mode 0)
-;;  (electric-indent-local-mode 0)
   )
 
 (provide 'setup-nim)
