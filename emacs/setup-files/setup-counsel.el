aebc8b19b84dedf6fd2edacf2aefabd610d93c0f
Arjun Kathuria <arjun.kathuria8@gmail.com>
2020-12-14 22:22:55 +0530

    adds ivy-rich mode to counsel. displays what commands do next to them

--- emacs/setup-files/setup-counsel.el
+++ emacs/setup-files/setup-counsel.el
@@ -1,9 +1,17 @@
 ;; Configuration for counsel, Ivy and Swiper
 
 ;;; Code:
+
+(use-package ivy
+  :ensure t)
+
+(use-package ivy-rich
+  :ensure t)
+
 (use-package counsel
   :init
   (ivy-mode 1)
+  (ivy-rich-mode 1)
   (setq ivy-use-virtual-buffers t
 	ivy-count-format "%d/%d ")
   (setq enable-recursive-minibuffers t)
@@ -12,7 +20,7 @@
 ;; ("C-s" . 'swiper)
    ("C-'" . 'swiper-avy)
    ("C-c C-r" . 'ivy-resume)
-;;   ("M-x" . 'counsel-M-x)
+   ("M-x" . 'counsel-M-x)
    ("C-x C-f" . 'counsel-find-file)
    ("C-c g" . 'counsel-git)
    ("C-c j" . 'counsel-get-grep)
