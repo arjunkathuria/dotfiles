88856f4f72d0406861d9a29943a8f63bbdc3013b
Arjun Kathuria <arjun.kathuria8@gmail.com>
2021-02-05 15:00:06 +0530

    updates haskell-mode config.

--- emacs/setup-files/setup-haskell.el
+++ emacs/setup-files/setup-haskell.el
@@ -3,14 +3,18 @@
 ;;; Code:
 (use-package haskell-mode
   :ensure t
-
+  :after company-mode
   :bind
   ("C-c c" . 'haskell-compile)
   ("C-c h" . 'haskell-check)
 
   :config
-  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-  (add-hook 'haskell-mode-hook #'lsp)
+  ;;(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  (add-hook 'haskell-mode-hook
+          (lambda ()
+            (set (make-local-variable 'company-backends)
+                 (append '((company-capf company-dabbrev-code))
+                         company-backends))))
   )
 
 (provide 'setup-haskell)
