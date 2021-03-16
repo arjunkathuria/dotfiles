32de1af699a195a56cbf14fb626fb3879e038c36
Arjun Kathuria <arjun.kathuria8@gmail.com>
2021-03-16 10:58:07 +0530

    remove lsp-mode hooks from nim and haskell modes.

--- emacs/setup-files/setup-lsp.el
+++ emacs/setup-files/setup-lsp.el
@@ -1,20 +1,13 @@
 (use-package lsp-mode
   :ensure t
-  :hook
-  (nim-mode . lsp)
-  (haskell-mode . lsp)
+  ;; :hook
+  ;; (nim-mode . lsp)
+  ;; (haskell-mode . lsp)
 
   :config
   (setq read-process-output-max (* 1024 1024)) ;; 1mb
   (setq lsp-completion-provider :capf)
 
-  (use-package lsp-haskell
-    :ensure t
-    ;;:hook (haskell-mode)
-    :config
-    ;;(add-hook 'haskell-mode-hook 'lsp-haskell)
-    (setq lsp-haskell-process-path-hie "hie-wrapper")
-    )
   (use-package lsp-ui
     :ensure t
     ;; :hook
