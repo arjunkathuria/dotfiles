d41020cb6732f379c708bb2fb22fb4ca8b6f18ee
Arjun Kathuria <arjun.kathuria8@gmail.com>
2021-03-16 10:57:23 +0530

    updates haskell-mode setup to add company dabbrev hook.

--- emacs/setup-files/setup-haskell.el
+++ emacs/setup-files/setup-haskell.el
@@ -3,18 +3,12 @@
 ;;; Code:
 (use-package haskell-mode
   :ensure t
-  :after company-mode
   :bind
   ("C-c c" . 'haskell-compile)
   ("C-c h" . 'haskell-check)
 
   :config
-  ;;(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-  (add-hook 'haskell-mode-hook
-          (lambda ()
-            (set (make-local-variable 'company-backends)
-                 (append '((company-capf company-dabbrev-code))
-                         company-backends))))
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
   )
 
 (provide 'setup-haskell)
