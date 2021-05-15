f040bc5dbdd9bdf5ecc6093777fb0f3f7ae2f685
Arjun Kathuria <arjun.kathuria8@gmail.com>
2021-05-15 23:09:12 +0530

    update some counsel keybindings, adds all-the-icons to ivy rich

--- emacs/setup-files/setup-counsel.el
+++ emacs/setup-files/setup-counsel.el
@@ -6,19 +6,28 @@
   :ensure t)
 
 (use-package ivy-rich
-  :ensure t)
+  :ensure t
+  :defer t)
+
+(use-package all-the-icons-ivy-rich
+  :ensure t
+  :defer t
+  :after all-the-icons
+  )
 
 (use-package counsel
   :init
   (ivy-mode 1)
   (ivy-rich-mode 1)
+  (all-the-icons-ivy-rich-mode 1)
   (setq ivy-use-virtual-buffers t
 	ivy-count-format "%d/%d ")
   (setq enable-recursive-minibuffers t)
+  
   :bind
   (
-;; ("C-s" . 'swiper)
-   ("C-'" . 'swiper-avy)
+   ("C-c C-s" . 'swiper)
+   ("C-c C-'" . 'swiper-avy)
    ("C-c C-r" . 'ivy-resume)
    ("M-x" . 'counsel-M-x)
    ("C-x C-f" . 'counsel-find-file)
@@ -26,6 +35,8 @@
    ("C-c j" . 'counsel-get-grep)
    ("C-c k" . 'counsel-ag)
    ("C-c l" . 'counsel-locate)
+   ("C-c f" . 'counsel-file-jump)
+   ("C-c SPC" . 'counsel-mark-ring)
    )
 )
 
