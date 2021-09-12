f8d078fc6c90d80cfd15d51284f39c8e49b0bdc2
Arjun Kathuria <arjun.kathuria8@gmail.com>
2021-09-12 22:31:18 +0530

    update setup-miniframe file

--- emacs/setup-files/setup-miniframe.el
+++ emacs/setup-files/setup-miniframe.el
@@ -5,12 +5,14 @@
 (use-package mini-frame
   :ensure t
   :custom
-  mini-frame-ignore-commands
-   '(ctrlf-forward-default)
-  mini-frame-detach-on-hide nil
-  mini-frame-show-parameters '((left . 0.5) (top . 0.25) (width . 0.5) (height . 1))
-  mini-frame-mode t
-  resize-mini-frames t
+
+  (mini-frame-ignore-commands
+   '(ctrlf-forward-default))
+  (mini-frame-detach-on-hide nil)
+  (mini-frame-show-parameters '((left . 0.5) (top . 0.25) (width . 0.5) (height . 1)))
+  (resize-mini-frames t)
+  (mini-frame-mode t)
+
 )
 
 (provide 'setup-miniframe)
