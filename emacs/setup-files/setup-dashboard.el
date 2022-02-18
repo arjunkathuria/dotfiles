bf9f492a8b98ef9b4e97a2ec38bed33015cea495
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-02-18 18:01:40 +0530

    Adds dashboard.el setup file and the cat icon png

---
+++ emacs/setup-files/setup-dashboard.el
@@ -0,0 +1,25 @@
+(require 'dashboard)
+
+;; Or if you use use-package
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  :custom
+  (dashboard-startup-banner (concat user-emacs-directory "themes/catppuccin.png"))
+  (dashboard-path-style 'truncate-beginning)
+  (dashboard-banner-logo-title "Arjun's dashboard!")
+  (dashboard-set-file-icons t)
+  (dashboard-set-init-info t)
+  (dashboard-center-content t)
+  (dashboard-set-heading-icons t)
+  (dashboard-projects-switch-function 'counsel-projectile-switch-project-by-name)
+  (dashboard-filter-agenda-entry 'dashboard-filter-agenda-by-time)
+  (dashboard-week-agenda t)
+  (dashboard-items '(
+                    (projects . 5)
+                    (agenda)
+                    (recents . 3))
+  ))
+
+(provide 'setup-dashboard)
