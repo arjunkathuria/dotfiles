864477deb4f8a0d2312e103d304ca0059a1fbc2e
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-03-07 08:16:54 +0000

    updates install.sh file with touchups

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ln -s "$PWD/git/.gitconfig" "$HOME/.gitconfig" &
-ln -s "$PWD/emacs/" "$HOME/.emacs.d"
-ln -s $PWD/config/*/ $HOME/.config/
+ln -s "$PWD/emacs/" "$HOME/.emacs.d" &
+ln -s $PWD/config/*/ $HOME/.config/ &
