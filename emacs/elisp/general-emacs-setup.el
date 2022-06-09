21dda8ebfee801b1061c2f06ca51b35fec405ba8
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-06-09 23:28:39 +0530

    overhaul general-emacs-setup file

--- emacs/elisp/general-emacs-setup.el
+++ emacs/elisp/general-emacs-setup.el
@@ -1,33 +1,47 @@
 ;;; Genral settings for emacs
 
-;; tabs to spaces
-(setq-default indent-tabs-mode nil)
-(setq tab-width 2)
+(use-package emacs
+  :init
+  ;; tabs to spaces
+  (setq-default indent-tabs-mode nil)
+  (setq tab-width 2)
 
-;;Fix the scrolling to keep point in the center:
-(setq scroll-conservatively 10000
-      scroll-preserve-screen-position t)
+  ;;Fix the scrolling to keep point in the center:
+  (setq scroll-conservatively 10000
+        scroll-preserve-screen-position t)
 
-;;Make tab key do indent first then completion.
-(setq-default tab-always-indent 'complete)
+  ;;Make tab key do indent first then completion.
+  (setq-default tab-always-indent 'complete)
 
-;;Relevant Apropos
-(setq apropos-sort-by-scores t)
-;;----------------------
+  ;;Relevant Apropos
+  (setq apropos-sort-by-scores t)
 
-;; GLOBAL enabled modes and hooks
-;;(require 'flycheck)
+  ;; Font settings
+  ;; set font-face
+  (set-face-attribute 'default nil :font "Source Code Pro" :height 115)
 
-;;(add-hook 'after-init-hook 'global-company-mode)
-(add-hook 'after-init-hook 'global-flycheck-mode)
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+  ;; Removes the side-scrollbars from emacs
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
 
-;; The VI like fringes in sidebar
-(use-package vi-tilde-fringe
-  :ensure t
-  :hook
-  (add-hook 'prog-mode-hook 'vi-tilde-fringe-mode)
-)
+  ;; Disable the annoying splash screen (to enable it agin, replace the t with 0)
+  (setq inhibit-splash-screen nil)
+
+  (column-number-mode t)
+  (line-number-mode t)
+
+  ;; uniform encoding across systems.
+  (set-language-environment "UTF-8")
+  ;;(set-buffer-file-coding-system 'utf-8-unix)
+
+  ;; windows set alt-gr (the right alt) as a normal alt.
+  ;; (setq w32-recognize-altgr nil)
+
+  ;; turn off backup files.
+  (setq make-backup-files nil)
+  (setq create-lockfiles nil)
+  (setq auto-save-default nil)
+  )
 
 ;;----------------------
 
@@ -38,46 +52,15 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ;;-------------------
 
-;; Font settings
-(if (eq system-type 'darwin)
-    ;;(set-face-attribute 'default nil :font "Operator Mono-13")
-    ;;(set-face-attribute 'default nil :font "DejaVu Sans Mono")
-    (set-face-attribute 'default nil :font "Hack-12"))
-
-(set-face-attribute 'default nil :font "Hack-12")
-
-;; Removes the side-scrollbars from emacs
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-;; Disable the annoying splash screen (to enable it agin, replace the t with 0)
-(setq inhibit-splash-screen nil)
-
-(column-number-mode t)
-(line-number-mode t)
-
-;; uniform encoding across systems.
-(set-language-environment "UTF-8")
-(set-buffer-file-coding-system 'utf-8-unix)
-
-(setq make-backup-files nil)
-(setq create-lockfiles nil)
-(setq auto-save-default nil)
-
-;; windows set alt-gr (the right alt) as a normal alt.
-(setq w32-recognize-altgr nil)
-
+;; load keymaps
 (require 'keymaps)
-(provide 'general-emacs-setup)
-
-;; turn off backup files.
-(setq make-backup-files nil)
 
 ;;; setup calender with week numbers
 (require 'calender)
 
+(provide 'general-emacs-setup)
 ;;; general-emacs-setup.el ends here
