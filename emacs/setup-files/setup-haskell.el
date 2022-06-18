09a219d770bc34e5eb9eeb39cd2138c6cd7d6162
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-06-18 18:15:59 +0530

    Misc updates to options in haskell-mode

--- emacs/setup-files/setup-haskell.el
+++ emacs/setup-files/setup-haskell.el
@@ -11,8 +11,9 @@
   ("C-c c" . 'haskell-compile)
   ("C-c h" . 'haskell-check)
 
-  ;;:hook
+  ;; :hook
   ;;interactive-haskell-mode
+  ;; '(flyspell-prog-mode highlight-uses-mode ormolu-format-on-save-mode interactive-haskell-mode)
 
   :config
   (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
@@ -21,27 +22,26 @@
   :custom
   ;; set interactive haskell mode variable settings
   (haskell-process-suggest-remove-import-lines t)
-  (haskell-process-auto-import-loaded-modules t)
+  (haskell-process-auto-import-loaded-modules nil)
   (haskell-process-log t)
   (haskell-process-prompt-restart-on-cabal-change t)
   (haskell-process-load-or-reload-prompt t)
-  (haskell-process-suggest-hoogle-imports t)
   (haskell-process-type 'auto)
-  (haskell-process-use-presentation-mode nil)
   (haskell-indent-offset 2)
 
   (haskell-process-args-cabal-repl '("--ghc-option=-ferror-spans" "--ghc-option=-Wall"))
   (haskell-process-args-ghci '("-ferror-spans" "-Wall"))
   (haskell-process-suggest-add-package nil)
   (haskell-process-suggest-hoogle-imports t)
-  (haskell-tags-on-save t)
+  (haskell-tags-on-save nil)
   (haskell-completing-read-function 'completing-read)
   (haskell-process-auto-import-loaded-modules nil)
-  (haskell-process-suggest-haskell-docs-imports t)
-  (haskell-process-suggest-hoogle-imports nil)
+  (haskell-process-suggest-haskell-docs-imports nil)
+  (haskell-process-suggest-hoogle-imports t)
   (haskell-process-suggest-remove-import-lines nil)
   (haskell-process-use-presentation-mode t)
- )
+  )
+
 (use-package ormolu
   ;; :hook (haskell-mode . ormolu-format-on-save-mode)
   :bind
