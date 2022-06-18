be211cfadec422e9861c0ee43d841ea7fc3705ec
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-06-18 18:16:50 +0530

    Updates undo-tree configs in its setup file

--- emacs/setup-files/setup-undo-tree.el
+++ emacs/setup-files/setup-undo-tree.el
@@ -4,6 +4,10 @@
 (use-package undo-tree
   :config
   (global-undo-tree-mode 1)
+
+  :custom
+  (undo-tree-auto-save-history nil)
+  (undo-tree-enable-undo-in-region t)
 )
 
 (provide 'setup-undo-tree)
