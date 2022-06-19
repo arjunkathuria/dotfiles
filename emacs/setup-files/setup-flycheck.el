6a7bad14af7ee7c7f5d5a00dba0720a180e93bb1
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-06-19 22:26:24 +0530

    Updates flycheck config

--- emacs/setup-files/setup-flycheck.el
+++ emacs/setup-files/setup-flycheck.el
@@ -1,10 +1,16 @@
 (use-package flycheck
   :init
-  (global-flycheck-mode)
+  ;;(global-flycheck-mode)
+  (add-hook 'after-init-hook 'global-flycheck-mode)
 
   :custom
   (flycheck-ghc-args '("-Wall"))
   (flycheck-haskell-ghc-executable "ghc")
   (flycheck-haskell-hlint-executable "hlint"))
+
+(use-package flyspell
+  :init
+  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+  (setq ispell-list-command "--list"))
 
 (provide 'setup-flycheck)
