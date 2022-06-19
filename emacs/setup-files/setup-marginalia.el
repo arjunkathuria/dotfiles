ad5ecacaf2ecfcb6a0fd750ebbc9589d15ed0820
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-06-19 22:24:58 +0530

    Update Marginalia config

--- emacs/setup-files/setup-marginalia.el
+++ emacs/setup-files/setup-marginalia.el
@@ -3,7 +3,16 @@
 ;;; Code:
 
 (use-package marginalia
-  :config
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  :init
   (marginalia-mode))
+
+(use-package all-the-icons-completion
+  :after (marginalia all-the-icons)
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
 
 (provide 'setup-marginalia)
