7d96a7c4fb7277361919c68eb6abeea376ba09a3
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-09-20 22:16:28 +0530

    Adds modus themes and its customizations

---
+++ emacs/setup-files/setup-modus-themes.el
@@ -0,0 +1,33 @@
+;; Configuration for custom theme i use
+
+;;; Code:
+(use-package modus-themes
+  :ensure
+  :custom
+  (modus-themes-bold-constructs t)
+  (modus-themes-italic-constructs t)
+  (modus-themes-syntax '(alt-syntax green-strings))
+  (modus-themes-mode-line '(moody accented borderless))
+  (modus-themes-tabs-accented t)
+
+  (modus-themes-completions
+      '((matches . (extrabold background))
+        (selection . (semibold accented))
+        (popup . (accented))))
+
+  (modus-themes-fringe 'subtle)
+  (modus-themes-lang-checkers '(text-also straight-underline))
+  (modus-themes-hl-line '(accented))
+  (modus-themes-subtle-line-numbers t)
+  (modus-themes-markup '(bold italic))
+  (modus-themes-paren-match '(bold))
+  (modus-themes-region '())
+
+  :config
+  (load-theme 'modus-operandi)
+  (load-theme 'modus-operandi)
+
+  (enable-theme 'modus-operandi))
+
+(provide 'setup-modus-themes)
+;;; setup-modus-themes.el ends here
