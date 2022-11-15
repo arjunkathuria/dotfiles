2b0be9dcafb09f34855e3058eccd4859b940ff4f
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-11-15 23:37:37 +0530

    Emacs: update pdf-tools config, change buffer encoding to utf8 from windows

--- emacs/setup-files/setup-pdf.el
+++ emacs/setup-files/setup-pdf.el
@@ -1,20 +1,21 @@
 (use-package pdf-tools
   :defer t
-  ;; :init  (pdf-tools-install)
+  ;; :straight (pdf-tools
+  ;;            :type git
+  ;;            :host github
+  ;;            :repo "vedang/pdf-tools")
+  :commands (pdf-view-mode pdf-tools-install)
+  :mode ("\\.[pP][dD][fF]\\'" . pdf-view-mode)
+  :magic ("%PDF" . pdf-view-mode)
   :config
   (pdf-tools-install)
-  (setq-default pdf-view-display-size 'fit-page)
-  )
-
-(setq TeX-view-program-selection '((output-pdf "PDF Tools"))
-        TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
-        TeX-source-correlate-start-server t)
-
-(add-hook 'TeX-after-compilation-finished-functions
-          #'TeX-revert-document-buffer)
+  (pdf-view-display-size 'fit-page)
+  (define-pdf-cache-function pagelabels)
+  :hook ((pdf-view-mode-hook . (lambda () (display-line-numbers-mode -1)))
+         (pdf-view-mode-hook . pdf-tools-enable-minor-modes)))
 
 (use-package org-pdftools
   :defer t
-  )
+  :hook (org-load-hook . org-pdftools-setup-link))
 
 (provide 'setup-pdf)
