b8461830594955325aa5ccc19b885013850fe466
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-12-31 15:27:01 +0530

    Emacs: Update some dashboard settings

--- emacs/setup-files/setup-dashboard.el
+++ emacs/setup-files/setup-dashboard.el
@@ -6,6 +6,7 @@
   (dashboard-setup-startup-hook)
   :custom
   (dashboard-startup-banner (concat user-emacs-directory "themes/catppuccin.png"))
+  (dashboard-image-banner-max-height 230)
   (dashboard-path-style 'truncate-beginning)
   (dashboard-banner-logo-title "Arjun's dashboard!")
   (dashboard-set-file-icons t)
@@ -13,12 +14,14 @@
   (dashboard-center-content t)
   (dashboard-set-heading-icons t)
   (dashboard-projects-switch-function 'counsel-projectile-switch-project-by-name)
+  (dashboard-projects-backend 'project-el)
+  (dashboard-projects-switch-function 'consult-projectile-switch-project)
   (dashboard-filter-agenda-entry 'dashboard-filter-agenda-by-time)
   (dashboard-week-agenda t)
   (dashboard-items '(
                     (projects . 5)
                     (agenda)
-                    (recents . 3))
+                    (recents . 5))
   ))
 
 (provide 'setup-dashboard)
